83625e9ecabf3cf17c1a7e145fbe989b8e3c6bbd
S-Luther <samluther998@gmail.com>
2023-09-06 15:46:34 -0600

    asd

---
+++ test/src/main/java/com/example/project/Hello.java
@@ -0,0 +1,16 @@
+package com.example.project;
+
+public class Hello {
+
+    static int addTwoTo(int i){
+        return i+2;
+    }
+
+    public static void main(String[] args){
+        int one = 1;
+
+        int three = addTwoTo(one); // returns 3
+    }
+
+}
+
